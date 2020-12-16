ab30104363cb56909eb240a6b0227435f3e8382f
Natan Rodrigues <natanrosilva@gmail.com>
2020-12-16 10:48:38 -0300

    Inserindo pedido

--- src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/ItemPedido.java
@@ -41,6 +41,19 @@ public class ItemPedido implements Serializable {
 		return id.getPedido();
 	}
 	
+	public Produto getProduto(Pedido pedido) {
+		return id.getProduto();
+	}
+	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
+	
+	
 	
 	public Produto getProduto() {
 		return id.getProduto();
