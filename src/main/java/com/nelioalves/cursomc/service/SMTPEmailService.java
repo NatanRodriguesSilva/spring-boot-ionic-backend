95e61682bb998cf0188ae43401e4733cb9d7d27e
Natan Rodrigues <natanrosilva@gmail.com>
2021-01-25 13:22:55 -0300

    Email HTML

--- src/main/java/com/nelioalves/cursomc/service/SMTPEmailService.java
+++ src/main/java/com/nelioalves/cursomc/service/SMTPEmailService.java
@@ -1,15 +1,21 @@
 package com.nelioalves.cursomc.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SMTPEmailService extends AbstractEmailService{
 
 	@Autowired
 	private MailSender mailSender;
+	
+	@Autowired
+	private JavaMailSender javaMailSender;
 	
 	private static final Logger LOG = LoggerFactory.getLogger(SMTPEmailService.class);
 	
@@ -17,6 +23,14 @@ public class SMTPEmailService extends AbstractEmailService{
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Enviando de email...");
 		mailSender.send(msg);
+		LOG.info("Email enviado");
+		
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Enviando de email html...");
+		javaMailSender.send(msg);
 		LOG.info("Email enviado");
 		
 	}
